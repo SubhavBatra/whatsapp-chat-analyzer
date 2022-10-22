7caf926890a8a6a9c6735f23d995b7ef4c01a523
SubhavBatra <sbatra_be20@thapar.edu>
2022-10-22 23:29:47 +0530

    note added

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 st.sidebar.title("Whatsapp Chat Analyzer")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader("Choose a text file")
+st.sidebar.header("Note:")
+st.sidebar.text("Use only 24 hour time format and \ndate in '%d/%m/%y' format \nin your text file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
